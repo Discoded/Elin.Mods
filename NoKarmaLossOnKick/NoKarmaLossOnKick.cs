946300bd7961f0034b3bde22a08e5db84d0a7b8f
Discoded <33738298+Discoded@users.noreply.github.com>
2024-11-27 15:45:34 -0800

    Add project files.

---
+++ NoKarmaLossOnKick/NoKarmaLossOnKick.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("discode.no_karma_loss_on_kick", "noKarmaLossOnKick", "1.0.0.0")]
+public class Mod_NoKarmaLossOnKick : BaseUnityPlugin
+{
+    private void Start()
+    {
+        var harmony = new Harmony("CharaPatch");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch]
+class CharaPatch
+{
+    [HarmonyPrefix, HarmonyPatch(typeof(Chara), nameof(Chara.Kick), new Type[] { typeof(Chara), typeof(bool), typeof(bool) })]
+    public static void Kick(Chara __instance, Chara t, bool ignoreSelf, ref bool karmaLoss)
+    {
+        //System.Console.WriteLine("Kick()------");
+        //System.Console.WriteLine(karmaLoss);
+        karmaLoss = false;
+        return;
+    }
+}
+
