da0764d020f2a8f901bc3625f58a12ec965d2381
Discoded <33738298+Discoded@users.noreply.github.com>
2024-12-02 12:12:39 -0800

    Added custom config to completely disable Kick option

--- NoKarmaLossOnKick/NoKarmaLossOnKick.cs
+++ NoKarmaLossOnKick/NoKarmaLossOnKick.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 
 [BepInPlugin("discode.no_karma_loss_on_kick", "noKarmaLossOnKick", "1.0.0.0")]
 public class Mod_NoKarmaLossOnKick : BaseUnityPlugin
 {
+    public static ConfigEntry<string> configKick;
+    public static ConfigEntry<bool> configDisplayKick;
+    private static Harmony harmony;
     private void Start()
     {
-        var harmony = new Harmony("CharaPatch");
+        configDisplayKick = Config.Bind("General.Toggles",
+                                        "DisplayKick",
+                                        true,
+                                        "true: enable kick option on action menu, false: hide kick option on action menu");
+        harmony = new Harmony("CharaPatch");
         harmony.PatchAll();
+    }
+    private void OnDestroy()
+    {
+        harmony.UnpatchSelf();
     }
 }
 
@@ -23,6 +35,18 @@ class CharaPatch
         //System.Console.WriteLine(karmaLoss);
         karmaLoss = false;
         return;
+    }
+
+    [HarmonyPrefix, HarmonyPatch(typeof(ActKick), nameof(ActKick.CanPerform), new Type[] { })]
+    public static bool CanPerform(ActKick __instance)
+    {
+        //System.Console.WriteLine("Kick()------");
+        //System.Console.WriteLine(karmaLoss);
+        bool DisplayKick = Mod_NoKarmaLossOnKick.configDisplayKick.Value;
+        if (!DisplayKick) {
+            return false;
+        }
+        return true;
     }
 }
 
